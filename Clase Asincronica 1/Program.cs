521ab5b3a43a797bd1d1d24a1869bc3224aa2e1e
ItsLean <lruiz@estudiantec.cr>
2025-03-15 19:15:06 -0600

    mas ejemplos

--- Clase Asincronica 1/Program.cs
+++ Clase Asincronica 1/Program.cs
@@ -10,11 +10,14 @@ class Program
         list.AddLast(20);
         list.AddLast(30);
         list.AddFirst(5);
-
+        list.AddFirst(11);
+        list.AddFirst(72);
+        list.AddFirst("15"); // Error de compilación
         Console.WriteLine("Lista después de agregar elementos:");
         list.PrintList();
 
         list.DeleteWithValue(20);
+        list.DeleteWithValue(5);
         Console.WriteLine("Lista después de eliminar el elemento 20:");
         list.PrintList();
     }
